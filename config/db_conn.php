ddd78cbefc7a003abe91211672f30b7a09713036
Olaniyan Foluso <olaniyanfoluso498@gmail.com>
2021-03-31 17:49:42 +0100

    connect to remote server

--- config/db_conn.php
+++ config/db_conn.php
@@ -1,14 +1,23 @@
 <?php
     // db conn class
     class Database {
-        // db parameters
-        private $host = "localhost";
-        private $db_name = "schooladvisor";
-        private $username = "root";
-        private $password = "freshpeter498";
+        // local db parameters
+        // private $host = "localhost";
+        // private $db_name = "schooladvisor";
+        // private $username = "root";
+        // private $password = "freshpeter498";
+        // private $conn;
+
+        // private $root_url = "http://localhost/phpsandbox/sidehustle-tasks/team6-dbcoll/";
+
+        // deployed db parameters
+        private $host = "remotemysql.com";
+        private $db_name = "KEqGNcRqrp";
+        private $username = "KEqGNcRqrp";
+        private $password = "6PHX00Mtmd";
         private $conn;
 
-        private $root_url = "http://localhost/phpsandbox/sidehustle-tasks/team6-dbcoll/";
+        private $root_url = "https://sh6-userdb.herokuapp.com/";
 
         //connect to db
         public function connect() {
